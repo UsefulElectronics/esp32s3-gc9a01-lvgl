f36d37f11c524ded9165d32818d8e69a5b84825f
unknown <wardzx@hotmail.com>
2023-01-30 12:53:13 -0800

    Added rotary encoder pulse counter initialization

---
+++ main/rotary_encoder.c
@@ -0,0 +1,122 @@
+/**
+ ******************************************************************************
+ *							USEFUL ELECTRONICS
+ ******************************************************************************/
+/**
+ ******************************************************************************
+ * @file    :  rotary_encoder.c
+ * @author  :  WARD ALMASARANI
+ * @version :  v.1.0
+ * @date    :  Jan 30, 2023
+ * @link    :  https://www.youtube.com/@usefulelectronics
+ *			   Hold Ctrl button and click on the link to be directed to
+			   Useful Electronics YouTube channel	
+ ******************************************************************************/
+
+
+/* INCLUDES ------------------------------------------------------------------*/
+#include "rotary_encoder.h"
+#include "stdbool.h"
+/* PRIVATE STRUCTRES ---------------------------------------------------------*/
+typedef struct
+{
+	uint8_t  whatchPoints[ENCODER_MAX_WHATCHPIONT_COINT];
+	uint32_t eventCount;
+	QueueHandle_t queue;
+	void (*callback)(uint32_t encoderValue);
+}encoder_handler_t;
+/* VARIABLES -----------------------------------------------------------------*/
+static const char *encoder = "encoder";
+encoder_handler_t hEncoder = {0};
+/* DEFINITIONS ---------------------------------------------------------------*/
+
+/* MACROS --------------------------------------------------------------------*/
+
+/* PRIVATE FUNCTIONS DECLARATION ---------------------------------------------*/
+static bool pcnt_on_reach(pcnt_unit_handle_t unit, const pcnt_watch_event_data_t *edata, void *user_ctx);
+
+/* FUNCTION PROTOTYPES -------------------------------------------------------*/
+static bool pcnt_on_reach(pcnt_unit_handle_t unit, const pcnt_watch_event_data_t *edata, void *user_ctx)
+{
+    BaseType_t high_task_wakeup;
+    QueueHandle_t queue = (QueueHandle_t)user_ctx;
+    // send event data to queue, from this interrupt callback
+    xQueueSendFromISR(queue, &(edata->watch_point_value), &high_task_wakeup);
+    return (high_task_wakeup == pdTRUE);
+}
+
+void encoder_handler_task(void *param)
+{
+    while (1)
+    {
+        if(xQueueReceive(hEncoder.queue, &hEncoder.eventCount, portMAX_DELAY))
+        {
+        	hEncoder.callback(hEncoder.eventCount);
+//            ESP_LOGI(TAG, "Watch point event, count: %d", event_count);
+        }
+    }
+}
+/**
+ * @brief 	Initialize pulse counter to interface rotary encoder.
+ *
+ * @param 	callback	: the passed callback will be executed
+ */
+void encoder_init(void* callback)
+{
+    ESP_LOGI(encoder, "install pcnt unit");
+    pcnt_unit_config_t unit_config = {
+        .high_limit = EXAMPLE_PCNT_HIGH_LIMIT,
+        .low_limit = EXAMPLE_PCNT_LOW_LIMIT,
+    };
+    pcnt_unit_handle_t pcnt_unit = NULL;
+    ESP_ERROR_CHECK(pcnt_new_unit(&unit_config, &pcnt_unit));
+
+    ESP_LOGI(encoder, "set glitch filter");
+    pcnt_glitch_filter_config_t filter_config = {
+        .max_glitch_ns = 1000,
+    };
+    ESP_ERROR_CHECK(pcnt_unit_set_glitch_filter(pcnt_unit, &filter_config));
+
+    ESP_LOGI(encoder, "install pcnt channels");
+    pcnt_chan_config_t chan_a_config = {
+        .edge_gpio_num = EXAMPLE_EC11_GPIO_A,
+        .level_gpio_num = EXAMPLE_EC11_GPIO_B,
+    };
+    pcnt_channel_handle_t pcnt_chan_a = NULL;
+    ESP_ERROR_CHECK(pcnt_new_channel(pcnt_unit, &chan_a_config, &pcnt_chan_a));
+    pcnt_chan_config_t chan_b_config = {
+        .edge_gpio_num = EXAMPLE_EC11_GPIO_B,
+        .level_gpio_num = EXAMPLE_EC11_GPIO_A,
+    };
+    pcnt_channel_handle_t pcnt_chan_b = NULL;
+    ESP_ERROR_CHECK(pcnt_new_channel(pcnt_unit, &chan_b_config, &pcnt_chan_b));
+
+    ESP_LOGI(encoder, "set edge and level actions for pcnt channels");
+    ESP_ERROR_CHECK(pcnt_channel_set_edge_action(pcnt_chan_a, PCNT_CHANNEL_EDGE_ACTION_DECREASE, PCNT_CHANNEL_EDGE_ACTION_INCREASE));
+    ESP_ERROR_CHECK(pcnt_channel_set_level_action(pcnt_chan_a, PCNT_CHANNEL_LEVEL_ACTION_KEEP, PCNT_CHANNEL_LEVEL_ACTION_INVERSE));
+    ESP_ERROR_CHECK(pcnt_channel_set_edge_action(pcnt_chan_b, PCNT_CHANNEL_EDGE_ACTION_INCREASE, PCNT_CHANNEL_EDGE_ACTION_DECREASE));
+    ESP_ERROR_CHECK(pcnt_channel_set_level_action(pcnt_chan_b, PCNT_CHANNEL_LEVEL_ACTION_KEEP, PCNT_CHANNEL_LEVEL_ACTION_INVERSE));
+
+    ESP_LOGI(encoder, "add watch points and register callbacks");
+    int watch_points[] = {EXAMPLE_PCNT_LOW_LIMIT, -50, 0, 50, EXAMPLE_PCNT_HIGH_LIMIT};
+    for (size_t i = 0; i < sizeof(watch_points) / sizeof(watch_points[0]); i++) {
+        ESP_ERROR_CHECK(pcnt_unit_add_watch_point(pcnt_unit, watch_points[i]));
+    }
+    pcnt_event_callbacks_t cbs = {
+        .on_reach = pcnt_on_reach,
+    };
+    hEncoder.queue = xQueueCreate(10, sizeof(int));
+    ESP_ERROR_CHECK(pcnt_unit_register_event_callbacks(pcnt_unit, &cbs, hEncoder.queue));
+
+    ESP_LOGI(encoder, "enable pcnt unit");
+    ESP_ERROR_CHECK(pcnt_unit_enable(pcnt_unit));
+    ESP_LOGI(encoder, "clear pcnt unit");
+    ESP_ERROR_CHECK(pcnt_unit_clear_count(pcnt_unit));
+    ESP_LOGI(encoder, "start pcnt unit");
+    ESP_ERROR_CHECK(pcnt_unit_start(pcnt_unit));
+
+    //External callback register.
+    hEncoder.callback = callback;
+}
+
+/*************************************** USEFUL ELECTRONICS*****END OF FILE****/
---
+++ main/rotary_encoder.h
@@ -0,0 +1,50 @@
+/**
+ ******************************************************************************
+ *							USEFUL ELECTRONICS
+ ******************************************************************************/
+/**
+ ******************************************************************************
+ * @file    :  rotary_encoder.h
+ * @author  :  WARD ALMASARANI
+ * @version :  v.1.0
+ * @date    :  Jan 30, 2023
+ * @link    :  https://www.youtube.com/@usefulelectronics
+ *			   Hold Ctrl button and click on the link to be directed to
+			   Useful Electronics YouTube channel	
+ ******************************************************************************/
+
+#ifndef MAIN_ROTARY_ENCODER_H_
+#define MAIN_ROTARY_ENCODER_H_
+
+
+/* INCLUDES ------------------------------------------------------------------*/
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "esp_log.h"
+#include "driver/pulse_cnt.h"
+/* MACROS --------------------------------------------------------------------*/
+
+
+#define ENCODER_MAX_WHATCHPIONT_COINT 100
+
+#define EXAMPLE_PCNT_HIGH_LIMIT 100
+#define EXAMPLE_PCNT_LOW_LIMIT  0
+
+#define EXAMPLE_EC11_GPIO_A 11
+#define EXAMPLE_EC11_GPIO_B 12
+/* ENUMORATIONS --------------------------------------------------------------*/
+
+/* STRUCTURES & TYPEDEFS -----------------------------------------------------*/
+
+/* VARIABLES -----------------------------------------------------------------*/
+
+/* FUNCTIONS DECLARATION -----------------------------------------------------*/
+void encoder_init(void* callback);
+
+void encoder_handler_task(void *param);
+
+
+#endif /* MAIN_ROTARY_ENCODER_H_ */
+
+/*************************************** USEFUL ELECTRONICS*****END OF FILE****/
