c1931296465b12e4267f273cd6452a7c2f1adb52
unknown <wardzx@hotmail.com>
2023-01-31 12:13:04 -0800

    working on code organization

---
+++ main/gc9a01.c
@@ -0,0 +1,103 @@
+/**
+ ******************************************************************************
+ *							USEFUL ELECTRONICS
+ ******************************************************************************/
+/**
+ ******************************************************************************
+ * @file    :  gc9a01.c
+ * @author  :  WARD ALMASARANI
+ * @version :  v.1.0
+ * @date    :  Jan 31, 2023
+ * @link    :  https://www.youtube.com/@usefulelectronics
+ *			   Hold Ctrl button and click on the link to be directed to
+			   Useful Electronics YouTube channel	
+ ******************************************************************************/
+
+
+/* INCLUDES ------------------------------------------------------------------*/
+#include "gc9a01.h"
+
+/* PRIVATE STRUCTRES ---------------------------------------------------------*/
+
+/* VARIABLES -----------------------------------------------------------------*/
+
+static const char *gc9a01 = "display";
+
+esp_lcd_panel_handle_t panel_handle = NULL;
+/* DEFINITIONS ---------------------------------------------------------------*/
+
+/* MACROS --------------------------------------------------------------------*/
+
+/* PRIVATE FUNCTIONS DECLARATION ---------------------------------------------*/
+
+/* FUNCTION PROTOTYPES -------------------------------------------------------*/
+void gc9a01_displayInit(void)
+{
+    ESP_LOGI(gc9a01, "Turn off LCD backlight");
+    gpio_config_t bk_gpio_config = {
+        .mode = GPIO_MODE_OUTPUT,
+        .pin_bit_mask = 1ULL << EXAMPLE_PIN_NUM_BK_LIGHT
+    };
+    ESP_ERROR_CHECK(gpio_config(&bk_gpio_config));
+
+    ESP_LOGI(gc9a01, "Initialize SPI bus");
+    spi_bus_config_t buscfg = {
+        .sclk_io_num = EXAMPLE_PIN_NUM_SCLK,
+        .mosi_io_num = EXAMPLE_PIN_NUM_MOSI,
+        .miso_io_num = EXAMPLE_PIN_NUM_MISO,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = EXAMPLE_LCD_H_RES * 80 * sizeof(uint16_t),
+    };
+    ESP_ERROR_CHECK(spi_bus_initialize(LCD_HOST, &buscfg, SPI_DMA_CH_AUTO));
+
+    ESP_LOGI(gc9a01, "Install panel IO");
+    esp_lcd_panel_io_handle_t io_handle = NULL;
+    esp_lcd_panel_io_spi_config_t io_config = {
+        .dc_gpio_num = EXAMPLE_PIN_NUM_LCD_DC,
+        .cs_gpio_num = EXAMPLE_PIN_NUM_LCD_CS,
+        .pclk_hz = EXAMPLE_LCD_PIXEL_CLOCK_HZ,
+        .lcd_cmd_bits = EXAMPLE_LCD_CMD_BITS,
+        .lcd_param_bits = EXAMPLE_LCD_PARAM_BITS,
+        .spi_mode = 0,
+        .trans_queue_depth = 10,
+        .on_color_trans_done = example_notify_lvgl_flush_ready,
+        .user_ctx = &disp_drv,
+    };
+    // Attach the LCD to the SPI bus
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)LCD_HOST, &io_config, &io_handle));
+
+    ESP_LOGI(gc9a01, "Install GC9A01 panel driver");
+
+    esp_lcd_panel_dev_config_t panel_config = {
+        .reset_gpio_num = EXAMPLE_PIN_NUM_LCD_RST,
+
+        .rgb_endian = LCD_RGB_ENDIAN_RGB,
+
+        .rgb_endian = LCD_RGB_ENDIAN_BGR,
+
+        .bits_per_pixel = 16,
+    };
+
+
+    ESP_ERROR_CHECK(esp_lcd_new_panel_gc9a01(io_handle, &panel_config, &panel_handle));
+
+
+    ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
+
+    ESP_ERROR_CHECK(esp_lcd_panel_invert_color(panel_handle, true));
+
+    ESP_ERROR_CHECK(esp_lcd_panel_mirror(panel_handle, true, false));
+
+    // user can flush pre-defined pattern to the screen before we turn on the screen or backlight
+    ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
+
+
+    ESP_LOGI(gc9a01, "Turn on LCD backlight");
+    gpio_set_level(EXAMPLE_PIN_NUM_BK_LIGHT, EXAMPLE_LCD_BK_LIGHT_ON_LEVEL);
+
+
+}
+
+/*************************************** USEFUL ELECTRONICS*****END OF FILE****/
---
+++ main/gc9a01.h
@@ -0,0 +1,74 @@
+/**
+ ******************************************************************************
+ *							USEFUL ELECTRONICS
+ ******************************************************************************/
+/**
+ ******************************************************************************
+ * @file    :  gc9a01.h
+ * @author  :  WARD ALMASARANI
+ * @version :  v.1.0
+ * @date    :  Jan 31, 2023
+ * @link    :  https://www.youtube.com/@usefulelectronics
+ *			   Hold Ctrl button and click on the link to be directed to
+			   Useful Electronics YouTube channel	
+ ******************************************************************************/
+
+#ifndef MAIN_GC9A01_H_
+#define MAIN_GC9A01_H_
+
+
+/* INCLUDES ------------------------------------------------------------------*/
+#include "esp_lcd_gc9a01.h"
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_timer.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_vendor.h"
+#include "esp_lcd_panel_ops.h"
+#include "driver/gpio.h"
+#include "driver/spi_master.h"
+
+#include "esp_err.h"
+#include "esp_log.h"
+/* MACROS --------------------------------------------------------------------*/
+// Using SPI2 in the example
+#define LCD_HOST  SPI2_HOST
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////// Please update the following configuration according to your LCD spec //////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#define EXAMPLE_LCD_PIXEL_CLOCK_HZ     (20 * 1000 * 1000)
+#define EXAMPLE_LCD_BK_LIGHT_ON_LEVEL  1
+#define EXAMPLE_LCD_BK_LIGHT_OFF_LEVEL !EXAMPLE_LCD_BK_LIGHT_ON_LEVEL
+#define EXAMPLE_PIN_NUM_SCLK           4//18
+#define EXAMPLE_PIN_NUM_MOSI           5//19
+#define EXAMPLE_PIN_NUM_MISO           16//21
+#define EXAMPLE_PIN_NUM_LCD_DC         6//5
+#define EXAMPLE_PIN_NUM_LCD_RST        15//3
+#define EXAMPLE_PIN_NUM_LCD_CS         7//4
+#define EXAMPLE_PIN_NUM_BK_LIGHT       18//2
+#define EXAMPLE_PIN_NUM_TOUCH_CS       17//15
+
+#define EXAMPLE_LCD_H_RES              240
+#define EXAMPLE_LCD_V_RES              240
+
+#define EXAMPLE_LCD_CMD_BITS           8
+#define EXAMPLE_LCD_PARAM_BITS         8
+
+
+
+/* ENUMORATIONS --------------------------------------------------------------*/
+
+/* STRUCTURES & TYPEDEFS -----------------------------------------------------*/
+
+/* VARIABLES -----------------------------------------------------------------*/
+extern esp_lcd_panel_handle_t panel_handle;
+/* FUNCTIONS DECLARATION -----------------------------------------------------*/
+
+
+
+#endif /* MAIN_GC9A01_H_ */
+
+/*************************************** USEFUL ELECTRONICS*****END OF FILE****/
