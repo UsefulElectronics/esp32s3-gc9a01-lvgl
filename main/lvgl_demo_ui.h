be1572050432cbf4062b7fbaa0bf5c60ef61786b
unknown <wardzx@hotmail.com>
2023-02-06 12:57:45 -0800

    Watch UI has been added

--- main/lvgl_demo_ui.h
+++ main/lvgl_demo_ui.h
@@ -26,9 +26,26 @@
 /* STRUCTURES & TYPEDEFS -----------------------------------------------------*/
 
 /* VARIABLES -----------------------------------------------------------------*/
+extern lv_obj_t * ui_Screen1;
+extern lv_obj_t * ui_Arc2;
+extern lv_obj_t * ui_Panel2;
+extern lv_obj_t * ui_Panel1;
+extern lv_obj_t * ui_Label2;
+extern lv_obj_t * ui_Label3;
+
+
+
+
+
+LV_FONT_DECLARE(ui_font_wise60);
+
+
 
 /* FUNCTIONS DECLARATION -----------------------------------------------------*/
 void set_value(int32_t v, bool buttonStatus);
+
+void ui_init(void);
+
 
 
 #endif /* MAIN_LVGL_DEMO_UI_H_ */
