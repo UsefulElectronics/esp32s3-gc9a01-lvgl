922920c88f604654eff6535c53bf7b2a2181994e
unknown <wardzx@hotmail.com>
2024-11-21 23:29:25 +0300

    Completed option structure

--- main/display/ui_page.c
+++ main/display/ui_page.c
@@ -20,7 +20,59 @@
 /* PRIVATE STRUCTRES ---------------------------------------------------------*/
 
 /* VARIABLES -----------------------------------------------------------------*/
+ui_option_t option_watch = 
+{
+	.title = "Time",
+	.actions = &(ui_action_t){.forward = NULL, .back = NULL, .idle = NULL },
+	.next_option = &option_measure,
+	.sub_option = NULL,
+	.icon = NULL,
+};
 
+ui_option_t option_measure = 
+{
+	.title = "Time",
+	.actions = &(ui_action_t){.forward = NULL, .back = NULL, .idle = NULL },
+	.next_option = &option_temperature,
+	.sub_option = NULL,
+	.icon = NULL,
+};
+
+ui_option_t option_temperature = 
+{
+	.title = "Time",
+	.actions = &(ui_action_t){.forward = NULL, .back = NULL, .idle = NULL },
+	.next_option = &option_weather,
+	.sub_option = NULL,
+	.icon = NULL,
+};
+
+ui_option_t option_weather = 
+{
+	.title = "Time",
+	.actions = &(ui_action_t){.forward = NULL, .back = NULL, .idle = NULL },
+	.next_option = &option_lamp1,
+	.sub_option = NULL,
+	.icon = NULL,
+};
+
+ui_option_t option_lamp1 = 
+{
+	.title = "Time",
+	.actions = &(ui_action_t){.forward = NULL, .back = NULL, .idle = NULL },
+	.next_option = &option_lamp2,
+	.sub_option = NULL,
+	.icon = NULL,
+};
+
+ui_option_t option_lamp2 = 
+{
+	.title = "Time",
+	.actions = &(ui_action_t){.forward = NULL, .back = NULL, .idle = NULL },
+	.next_option = NULL,
+	.sub_option = NULL,
+	.icon = NULL,
+};
 /* DEFINITIONS ---------------------------------------------------------------*/
 
 /* MACROS --------------------------------------------------------------------*/
--- main/display/ui_page.h
+++ main/display/ui_page.h
@@ -20,24 +20,35 @@
 /* INCLUDES ------------------------------------------------------------------*/
 #include <stdio.h>
 /* MACROS --------------------------------------------------------------------*/
-
+#define OPTION_BUFFER_SIZE		30
 /* ENUMORATIONS --------------------------------------------------------------*/
 
 /* STRUCTURES & TYPEDEFS -----------------------------------------------------*/
-typedef struct page
+typedef struct action
+{
+	void (*forward)();
+    void (*back)();
+    void (*idle)();
+}ui_action_t;
+
+typedef struct option
 {
 	char *title;
-    void (*page_init)();
-    void (*page_exit)();
-    void (*page)();
-    struct page *sub_menu;
-    struct page *next_menu;
+	struct option *sub_option;
+    struct option *next_option;
+    ui_action_t *actions;
+	uint8_t buffer[OPTION_BUFFER_SIZE];
     void *icon;
     uint8_t reserved : 8;
 
-}page_t;
+}ui_option_t;
 /* VARIABLES -----------------------------------------------------------------*/
-
+ui_option_t option_watch;
+ui_option_t option_measure;
+ui_option_t option_temperature;
+ui_option_t option_weather;
+ui_option_t option_lamp1;
+ui_option_t option_lamp2;
 /* FUNCTIONS DECLARATION -----------------------------------------------------*/
 
 
